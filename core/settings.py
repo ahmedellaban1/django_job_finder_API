2b38a762a8daffaae6f560ef37e6a4524f6c4bd7
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-30 02:04:26 +0200

    add rest-framework default settings

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # installed modules, apps
     'rest_framework',
+    'django_filters',
 
     # my apps
     'job',
@@ -133,3 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50
+}
