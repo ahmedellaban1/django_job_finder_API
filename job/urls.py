9d4dd81540f55d582e33118667ad5649518e5fae
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-10-30 21:30:23 +0200

    update urls

--- job/urls.py
+++ job/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import job_list, job_details
 
 urlpatterns = [
-    path('', job_list, name='job_list_API_view'),
-    path('<int:id>', job_details, name='job_details_API_view')
+    path('list', job_list, name='job_list_API_view'),
+    path('list/<int:id>', job_details, name='job_details_API_view')
 ]
